6542c2f370098e81bbec84c55a38fa59bdd5f65c
Rob Waggoner <rwagg@robhome.com>
2020-09-02 16:05:16 -0500

    Fixed pylint

--- RVtemp.py
+++ RVtemp.py
@@ -107,14 +107,17 @@ def get_RVTemp():
                         sendEmail = True
                 else :
                     sendEmail = True
-
+            #pylint: disable=too-many-arguments
             temperature = "{:.0f}".format(numTemp,1)
+            #pylint: enable=too-many-arguments
             message += name + ' ' + temperature + '\n'
             
 # This does assume you only have a single generator
         message += "Batt V " + str(generator_details['Battery Voltage']) + '\n'
         message += 'Gen State ' + generator_details['Running State'] + '\n'
+        #pylint: disable=too-many-arguments
         level = "{:.0f}".format(fuel_tank["Fuel Tank"]["Fuel"],0)
+        #pylint: enable=too-many-arguments
         message += 'Fuel ' + level + "% \n"
         message += "Hi limit " + tempHi + '\n'
         message += "Low limit " + tempLow 
